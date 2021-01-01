c4d689087f2cdbc2d8163dea0da5498551569c07
huyphan222 <huyphan2513@gmail.com>
2021-01-01 20:16:34 +0700

    updatebai

---
+++ CauTrucDuLieuStackQueue/BTDaoNguocPhanTuBangStack/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BTDaoNguocPhanTuBangStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack stackList = new MyStack(5);
+            stackList.Push(2);
+            stackList.Push(3);
+            stackList.Push(5);
+            stackList.Push(10);
+            stackList.Push(12);            
+            Console.WriteLine("1. Size of stack after push operations: " + stackList.Size());
+
+            while (!stackList.IsEmpty())
+            {
+                Console.WriteLine(stackList.Pop());
+            }
+
+            Console.WriteLine("\n3. Size of stack after pop operations : " + stackList.Size());
+         
+            
+        }
+    }
+}
